3b6a4b6479e920073f73e5c1889cf1ba22a696fa
eugenefrg <eugene261997@gmail.com>
2022-08-17 10:52:11 +0800

    added own image

--- src/components/bio.js
+++ src/components/bio.js
@@ -36,7 +36,7 @@ const Bio = () => {
         className="bio-avatar"
         layout="fixed"
         formats={["auto", "webp", "avif"]}
-        src="../images/profile-pic.png"
+        src="https://avatars.githubusercontent.com/u/21172879?v=4"
         width={50}
         height={50}
         quality={95}
